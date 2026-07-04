a2c092b46c5a1a3a0fb27c973d3bc9c52a50af12
nicogo.eth <Nicogo1705@users.noreply.github.com>
2026-07-04 15:47:51 +0200

    chore: back out copyright-header fill and dependabot (deferred by request)

--- src/AssetStore.Core/Validation/SchemaValidator.cs
+++ src/AssetStore.Core/Validation/SchemaValidator.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 Nicogo
+// Copyright (c) <YEAR> <COPYRIGHT HOLDER>
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
 using System.Collections.Concurrent;
